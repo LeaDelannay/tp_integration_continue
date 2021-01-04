bbe77b0a5140ce74201b2327dfdd07cd31338e7a
Léa DELANNAY <lea.delannay@pole-emploi.fr>
2021-01-04 16:45:25 +0100

    ajout classe de test calcul

--- src/main/java/Calculs.java
+++ src/main/java/Calculs.java
@@ -1,3 +1,5 @@
+package main.java;
+
 public class Calculs
 {
         private final int operande1;
---
+++ src/test/java/CalculsTest.java
@@ -0,0 +1,37 @@
+package test.java;
+
+import main.java.Calculs;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculsTest {
+
+    private Calculs c1 = null;
+    private Calculs c2 = null;
+    private Calculs c3 = null;
+
+    /**
+     * Initialise les valeurs avant chaque test
+     */
+
+    @BeforeEach
+    void setUp() throws Exception{
+        c1 = new Calculs(1,2);
+        c2 = new Calculs(10,20);
+        c3 = new Calculs(100,200);
+    }
+
+    /**
+     * Test method for {@link Calculs#multiplier()}.
+     */
+    @Test
+    void testMultiplier(){
+        if(c1.multiplier() != 2)
+        {
+            fail("Methode multiplier non conforme 2*1=2 ne fonctionne pas.");
+        }
+        assertEquals(c1.multiplier(), 2);
+    }
+}
